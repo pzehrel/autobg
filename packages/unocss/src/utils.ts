4a0bdc389441993d3ede9e45191e1779611c8df6
pzehrel <pzehrel@gmail.com>
2025-03-31 17:12:02 +0800

    refactor: remove deprecated absoluteToAliasPath function

--- packages/unocss/src/utils.ts
+++ packages/unocss/src/utils.ts
@@ -1,6 +1,3 @@
-import type { AliasConfig } from '@autobg/shared'
-import { join } from 'node:path'
-
 /**
  * Normalize the path of the url() function
  *
@@ -11,20 +8,4 @@ import { join } from 'node:path'
  */
 export function normalizePath(path: string) {
   return path.replace(/^\[?(?:url\(?)?['"]?(.+?)['"]?\)?\]?$/, '$1')
-}
-
-/**
- * @deprecated
- */
-export function absoluteToAliasPath(path: string, root: string, aliasConfig: AliasConfig, aliasSymbol?: string) {
-  // path = normalize(path.replace(root, ''))
-
-  const alias = aliasSymbol ? Object.entries(aliasConfig).find(([key]) => key === aliasSymbol) : undefined
-  if (alias) {
-    const [symbol, aliasPath] = alias
-    path = path.replace(new RegExp(`^/${aliasPath}`), '')
-    path = join(symbol, path)
-  }
-
-  return path
 }
