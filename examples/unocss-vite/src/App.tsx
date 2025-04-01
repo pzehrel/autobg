a4a1c539a1a3b0b5911eb543a8c2463dc3fc74bb
pzehrel <pzehrel@gmail.com>
2025-04-01 14:08:09 +0800

    fix: update logo paths and add vite logo asset

--- examples/unocss-vite/src/App.tsx
+++ examples/unocss-vite/src/App.tsx
@@ -8,10 +8,10 @@ function App() {
           <div className="logo autobg-[url('/vite.png')] inline-block"></div>
         </a>
         <a href="https://react.dev" target="_blank">
-          <div className="logo autobg-[url('./assets/react.png')] inline-block"></div>
+          <div className="logo autobg-[url(./assets/react.png)] inline-block"></div>
         </a>
         <a href="https://react.dev" target="_blank">
-          <div className="logo autobg-[url('@/assets/react.png')] inline-block"></div>
+          <div className="logo autobg-[url('@/assets/vite.png')] inline-block"></div>
         </a>
       </div>
       <h1>Vite + React</h1>
