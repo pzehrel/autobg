3fcbd403dae61bd2aacfe6f8317e6eff88bc8e0f
pzehrel <pzehrel@gmail.com>
2025-04-16 17:40:47 +0800

    chore: update eslint configuration and add eslint-plugin-format dependency

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,12 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   type: 'lib',
   typescript: true,
+  markdown: false,
+  stylistic: true,
+
+  formatters: {
+    markdown: true,
+  },
 
   rules: {
     'ts/explicit-function-return-type': 'off',
